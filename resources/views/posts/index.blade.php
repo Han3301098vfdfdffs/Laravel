93a84d7f3a295a8e9c5e230f2d8594b05a3059f0
Carlos Haniel Soriano Martinez <haniel.sor.3301@gmail.com>
2025-10-22 11:13:30 -0600

    Directivas Environments

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,31 +8,15 @@
     </head>
     <body>
         <h1>Aquí se mostrará el listado de posts</h1>
+        <p>Este es un parrafo</p>
 
-        @if(false)
+        @env('local')
+            <p>Estamos en local</p>
+        @endenv
 
-            <p>Este mensaje se mostrará sí el valor de la condicional es verdadera</p>
-
-        @else
-
-            <p>Este mensaje se mostrará sí el valor de la condicional es falsa</p>
-
-        @endif
-
-        @unless (true)
-            <p>Le has pasado el valor de false a la directiva unless</p>
-        @endunless
-
-        @isset($prueba45)
-            <p>La variable existe y tiene un valor asignado</p>
-        @else
-            <p>La variable no existe o no tiene un valor asignado</p>
-            
-        @endisset
-
-        @empty($prueba45)
-            <p>La variable no existe o no tiene un valor asignado</p>
-        @endempty
+        @production
+            <p>Estamos en production</p>   
+        @endproduction
 
     </body>
 </html>
